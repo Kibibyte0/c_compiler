0b6e0ec061ab0fc7c21673b0cbab6458ff9667f7
kibibyte0 <yasqad57@gmail.com>
2025-09-26 17:22:04 +0300

    Refactor the lexer

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,7 +1,28 @@
 use logos::Logos;
 
+// Extras for logos to keep track of line number
+#[derive(Debug, Default)]
+pub struct LinePosition {
+    line_num: usize,
+}
+
+// callback function to be used whenever logos match a new line char
+// always return None to skip the newline
+fn update_line_num(lex: &mut logos::Lexer<Token>) {
+    lex.extras.line_num += 1;
+}
+
 #[derive(Debug, PartialEq, Logos)]
+#[logos(extras = LinePosition)]
 pub enum Token {
+    // Identifiers: starts with a letter or underscore, followed by letters, digits, or underscores
+    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*", priority = 0)]
+    Identifier,
+
+    // Integer constants (decimal only)
+    #[regex(r"\d+")]
+    ConstantInt,
+
     // Keywords
     #[token("return")]
     Return,
@@ -28,127 +49,80 @@ pub enum Token {
     #[token(";")]
     Semicolon,
 
-    // Catch invalid tokens like "9main"
-    #[regex(r"\d+[a-zA-Z_][a-zA-Z0-9_]*")]
-    Invalid,
-
-    // Integer constants (decimal only)
-    #[regex(r"\d+")]
-    ConstantInt,
-
-    // Identifiers: starts with a letter or underscore, followed by letters, digits, or underscores
-    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*", priority = 0)]
-    Identifier,
-
     // skipped patterns
-    #[regex(r"[ \t\n\f]+", logos::skip)]
-    #[regex(r"//[^\n]*", logos::skip)]
-    #[regex(r"/\*[^*]*\*+([^/*][^*]*\*+)*/", logos::skip)]
+    #[regex(r"\n", callback = update_line_num)]
+    #[regex(r"[ \t\f]+")]
+    #[regex(r"//[^\n]*")]
+    #[regex(r"/\*[^*]*\*+([^/*][^*]*\*+)*/")]
+    Skip,
+
+    // invalid patterns
+    #[regex(r"\d+[a-zA-Z_][a-zA-Z0-9_]*")]
     Error,
 } 
 
 
-pub struct Lexer<'source> {
-    iter: logos::Lexer<'source, Token>,
-    input: &'source str,
-    line: usize,
-    column: usize,
-    position: usize,
-}
-
-pub struct Span {
+#[derive(Debug)]
+pub struct SpannedToken<'source> {
+    pub token_type: Token,
+    pub lexeme: &'source str,
     pub line_num: usize,
     pub col_start: usize,
     pub col_end: usize,
 }
 
-pub struct SpannedToken<'source> {
-    pub token_type: Token,
-    pub lexeme: &'source str,
-    pub span: Span,
-}
-
 impl<'source> Default for SpannedToken<'source> {
     fn default() -> Self {
         Self {
-            token_type: Token::Invalid,
-            lexeme: "exmpty",
-            span: Span { line_num: 0, col_start: 0, col_end: 0 }
+            token_type: Token::Skip,
+            lexeme: "",
+            line_num: 0,
+            col_start: 0,
+            col_end: 0,
         }
     }
 }
 
+pub struct Lexer<'source> {
+    lex: logos::Lexer<'source, Token>,
+}
+
 impl<'source> Lexer<'source> {
-    // initiate the lexer
     pub fn new(input: &'source str) -> Self {
+        let extras = LinePosition { line_num: 1 };
+
         Self {
-            iter: Token::lexer(input),
-            input,
-            line: 1,
-            column: 0,
-            position: 0,
+            lex: Token::lexer_with_extras(input, extras),
         }
     }
 
-    pub fn next(&mut self) -> Option<SpannedToken<'source>> {
-        // return None when the iter is empty
-        let token_type = match self.iter.next()? {
-            Ok(mat) => mat,
-            Err(_) => panic!("invalid token: {}", self.iter.slice()),
-        };
+pub fn next(&mut self) -> Option<SpannedToken<'source>> {
+        loop {
+            let token = match self.lex.next()? {
+                Ok(tok) => tok,
+                _ => Token::Error,
+            };
 
-        // catch invalid tokens
-        if let Token::Invalid = token_type {
-            panic!("Inavlid Identifier {}", self.iter.slice());
-        }
-
-        // update the line and column number
-        let start = self.iter.span().start;
-        self.count_lines(start);
-
-        // update the current position
-        self.position = self.iter.span().end;
-
-        // set the start of the token and end of the token relative to the line 
-        let token_column_start = self.column;
-        self.column += self.position - start;
-        let token_column_end = self.column;
-
-        let spanned_token = SpannedToken {
-            token_type,
-            lexeme: self.iter.slice(),
-            span: Span {
-                line_num: self.line,
-                col_start: token_column_start,
-                col_end: token_column_end
-            },
-        };
-
-        Some(spanned_token)
-    }
-
-    // count all the lines and columns from the last position
-    fn count_lines(&mut self, start: usize) {
-        for char in self.input[self.position..start].chars() {
-            if char == '\n' {
-                self.line += 1;
-                self.column = 0;
-            } else {
-                self.column += 1;
+            match token {
+                Token::Skip => continue,
+                _ => {
+                    return Some(SpannedToken {
+                        token_type: token,
+                        lexeme: self.lex.slice(),
+                        line_num: self.lex.extras.line_num,
+                        col_start: self.lex.span().start,
+                        col_end: self.lex.span().end,
+                    });
+                }
             }
         }
     }
 
-    // getters for priveate fields
-    pub fn get_source_code(&self) -> &'source str {
-        self.input
-    }
-
     pub fn get_line_num(&self) -> usize {
-        self.line
+        self.lex.extras.line_num
     }
 
-    pub fn get_col_num(&self) -> usize {
-        self.column
+    pub fn get_span(&self) -> std::ops::Range<usize> {
+        self.lex.span()
     }
 }
