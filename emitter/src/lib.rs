205933dcb948ec02d1d888156bba2038493fa7b7
kibibyte0 <yasqad57@gmail.com>
2025-10-01 10:33:20 +0300

    Rewrite the code emitter

--- emitter/src/lib.rs
+++ emitter/src/lib.rs
@@ -1,80 +1,129 @@
-// use std::{fs::File, path::PathBuf};
-// use std::fmt::{self, Write as FmtWrite};
-// use std::io::Write as IoWrite;
-// use codegen::asm_ast::Operand;
+use codegen::asm_ast;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
 
-// use codegen::asm_ast::{AsmFunctionDefinition, AsmProgram, Instruction};
+pub struct Emitter {
+    opcode_width: usize,
+    operand_width: usize,
+    indent: String,
+}
 
-// pub struct Emitter<'source> {
-//     program: AsmProgram<'source>,
-//     output_code: String,
-//     file_path: PathBuf,
-// }
+impl Emitter {
+    pub fn new(opcode_width: usize, operand_width: usize, indent_level: usize) -> Self {
+        // each indentation level will be four spaces
+        let indent = "    ".repeat(indent_level);
+        Self {
+            opcode_width,
+            operand_width,
+            indent,
+        }
+    }
 
-// impl<'source> Emitter<'source> {
-//     pub fn new(program: AsmProgram<'source>, file_path: PathBuf) -> Self {
-//         Self {
-//             program,
-//             output_code: String::new(),
-//             file_path,
-//         }
-//     }
+    pub fn write_program(
+        &self,
+        program: asm_ast::Program,
+        output_file_path: PathBuf,
+    ) -> std::io::Result<()> {
+        let mut file = File::create(output_file_path)?;
+        let mut code = String::new();
 
-//     // write to the output string, and panic if the operation fails
-//     fn emit(buffer: &mut impl FmtWrite, s: impl fmt::Display) {
-//         writeln!(buffer, "{}", s)
-//             .expect("error: failed to write output code");
-//     }
+        let function = program.into_parts();
 
-//     pub fn emit_asm(&mut self) {
-//         self.write_program();
-//         let mut file = File::create(&self.file_path)
-//             .expect("error: failed to create the output file");
-//         file.write_all(self.output_code.as_bytes()).unwrap();
-//     }
+        self.write_function_def(function, &mut code)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
 
-//     fn write_program(&mut self) {
-//         Emitter::write_function_def(&mut self.output_code, &self.program.function);
-//         Emitter::emit(
-//             &mut self.output_code,
-//             format!("   .section .note.GNU-stack,\"\",@progbits")
-//         );
-//     }
+        self.write_program_epilogue(&mut code);
+        file.write_all(code.as_bytes())?;
+        Ok(())
+    }
 
-//     fn write_function_def(output_code: &mut String, function: &AsmFunctionDefinition) {
-//         Emitter::emit(output_code, format!("   .globl {}", function.name));
-//         Emitter::emit(output_code, format!("{}:", function.name));
-//         Emitter::write_instructions(output_code, &function.instructions);
-//     }
+    fn write_program_epilogue(&self, code: &mut String) {
+        let s = format!("{}.section .note.GNU-stack,\"\",@progbits\n", self.indent);
+        code.push_str(&s);
+    }
 
-//     fn write_instructions(output_code: &mut String, instructions: &Vec<Instruction>) {
-//         for instruction in instructions {
-//             Emitter::write_instruction(output_code, instruction);
-//         }
-//     }
+    fn write_function_def(
+        &self,
+        function: asm_ast::FunctionDef,
+        code: &mut String,
+    ) -> std::fmt::Result {
+        let (name, instructions) = function.into_parts();
+        self.write_function_def_prolouge(code, name);
 
-//     fn write_instruction(output_code: &mut String, instruction: &Instruction) {
-//         match instruction {
-//             Instruction::Mov(src, dst) => Emitter::write_mov(output_code, src, dst),
-//             Instruction::Ret => Emitter::write_ret(output_code),
-//         }
-//     }
+        for instr in instructions {
+            self.write_instruction(instr, code)?;
+        }
 
-//     fn write_mov(output_code: &mut String, src: &Operand, dst: &Operand) {
-//         Emitter::emit(output_code, format!("    movl    {}, {}",
-//             Emitter::fetch_operand(src),
-//             Emitter::fetch_operand(dst),
-//         ));
-//     }
+        Ok(())
+    }
 
-//     fn fetch_operand(op: &Operand) -> String {
-//         match op {
-//             Operand::Immediate(int) => format!("${}", int),
-//             Operand::Register => "%eax".to_string(),
-//         }
-//     }
+    fn write_function_def_prolouge(&self, code: &mut String, name: asm_ast::Identifier) {
+        code.push_str(&format!("{}.globl {}\n", self.indent, name.0));
+        code.push_str(&format!("{}:\n", name.0));
 
-//     fn write_ret(output_code: &mut String) {
-//         Emitter::emit(output_code, format!("    ret"));
-//     }
-// }
+        let instr1 = self.format_one_operand_instruction("pushq", "%rsp");
+        code.push_str(&format!("{}", instr1));
+
+        let instr2 = self.format_two_operand_instruction("movq", "%rsp,", "%rbp");
+        code.push_str(&format!("{}", instr2));
+    }
+
+    fn write_instruction(
+        &self,
+        instr: asm_ast::Instruction,
+        out: &mut dyn std::fmt::Write,
+    ) -> std::fmt::Result {
+        match instr {
+            asm_ast::Instruction::Mov { dst, src } => {
+                let mut src = src.as_x86();
+                src.push(',');
+
+                let instr = self.format_two_operand_instruction("movl", &src, &dst.as_x86());
+                write!(out, "{}", instr)
+            }
+            asm_ast::Instruction::Unary { operator, operand } => {
+                let instr =
+                    self.format_one_operand_instruction(&operator.as_x86(), &operand.as_x86());
+
+                write!(out, "{}", instr)
+            }
+            asm_ast::Instruction::AllocateStack(size) => {
+                let src = format!("${size},");
+
+                let instr = self.format_two_operand_instruction("subq", &src, "%rsp");
+                write!(out, "{}", instr)
+            }
+
+            asm_ast::Instruction::Ret => {
+                let instr1 = self.format_two_operand_instruction("movq", "%rbp,", "%rsp");
+                let instr2 = self.format_one_operand_instruction("popq", "&rbp");
+                write!(out, "{}", instr1)?;
+                write!(out, "{}", instr2)?;
+                write!(out, "{}ret\n", self.indent)
+            }
+        }
+    }
+
+    fn format_one_operand_instruction(&self, operator: &str, operand: &str) -> String {
+        format!(
+            "{}{:<opcode_width$} {}\n",
+            self.indent,
+            operator,
+            operand,
+            opcode_width = self.opcode_width
+        )
+    }
+
+    fn format_two_operand_instruction(&self, operator: &str, op1: &str, op2: &str) -> String {
+        format!(
+            "{}{:<opcode_width$} {:<operand_width$} {}\n",
+            self.indent,
+            operator,
+            op1,
+            op2,
+            opcode_width = self.opcode_width,
+            operand_width = self.operand_width
+        )
+    }
+}
